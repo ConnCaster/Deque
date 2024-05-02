a65acfbfe96f8ca397bad162fdd6d5b48fcbf4aa
ConnCaster <example@example.com>
2024-05-02 15:23:49 +0300

    remove deps_sources

--- deps_sources/Catch2/OUT/src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o.d
+++
@@ -1,4 +0,0 @@
-src/CMakeFiles/Catch2.dir/catch2/internal/catch_result_type.cpp.o: \
- /home/user/dir/programming/C++/Alexey_CPP/Catch2/src/catch2/internal/catch_result_type.cpp \
- /usr/include/stdc-predef.h \
- /home/user/dir/programming/C++/Alexey_CPP/Catch2/src/catch2/../catch2/internal/catch_result_type.hpp
