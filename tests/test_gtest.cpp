040601f0dc587e08d91f337406c106b3bc3849af
ConnCaster <example@example.com>
2024-05-05 15:53:34 +0300

    stack compare

--- tests/test_gtest.cpp
+++ tests/test_gtest.cpp
@@ -199,7 +199,7 @@ TEST(DequeOperators, Assignment) {
 
     EXPECT_EQ(first.size(), 9);
     EXPECT_EQ(first.size(), second.size());
-//    EXPECT_TRUE(CompareStacks(first, second));
+    EXPECT_TRUE(CompareStacks(first, second));
 }
 
 TEST(DequeConstructors, StaticAsserts) {
@@ -448,7 +448,6 @@ TEST(Deque, Throw) {
 
     {
         Deque<ThrowStruct> d(10, ThrowStruct(10, true, false));
-//        d[0] = ThrowStruct(1, false, false);
         EXPECT_THROW(d[0] = ThrowStruct(1, false, false), int);
         EXPECT_EQ(d.size(), 10);
         EXPECT_EQ(d[0].value, 10);
@@ -461,7 +460,88 @@ TEST(Deque, Throw) {
     }
 }
 
+
+#include <algorithm>
+#include <vector>
+#include <chrono>
+#include <iostream>
+
+void FillVectorWithRandomNumbers(std::vector<size_t>& v,
+                                 size_t numbers_count,
+                                 size_t begin,
+                                 size_t end) {
+    v.resize(numbers_count);
+
+    std::random_device rnd_device;
+    std::mt19937 mersenne_engine{rnd_device()};
+    std::uniform_int_distribution<size_t> dist{begin, end};
+
+    auto gen = [&dist, &mersenne_engine]() {
+        return dist(mersenne_engine);
+    };
+
+    std::generate(v.begin(), v.end(), gen);
+}
+
+/*void TestFunction(const std::vector<size_t>& test_vector) {
+    Deque<size_t> d;
+
+    for (const auto& number: test_vector) {
+        d.push_back(number);
+    }
+
+    for (const auto& number: test_vector) {
+        d.push_front(number);
+    }
+
+    while (!d.empty()) {
+        d.pop_back();
+    }
+}*/
+
+static constexpr size_t kTestSize = 10000000;
+static constexpr size_t kDistrBegin = 1;
+static constexpr size_t kDistrEnd = 100;
+static constexpr long long kNormalDuration = 5;
+
+/*int main() {
+    std::vector<size_t> vector_with_random_numbers;
+    FillVectorWithRandomNumbers(vector_with_random_numbers,
+                                kTestSize,
+                                kDistrBegin,
+                                kDistrEnd);
+
+    auto start = std::chrono::high_resolution_clock::now();
+    TestFunction(vector_with_random_numbers);
+    auto stop = std::chrono::high_resolution_clock::now();
+
+    auto duration_in_seconds =
+            std::chrono::duration_cast<std::chrono::seconds>(stop - start).count();
+
+    std::cout << "Stress test took " << duration_in_seconds << " seconds" << std::endl;
+
+    return duration_in_seconds > kNormalDuration ? 1 : 0;
+}*/
+
+
+
+
 int main(int argc, char** argv) {
     testing::InitGoogleTest(&argc, argv);
+    //================ STRESS =====================
+//    std::vector<size_t> vector_with_random_numbers;
+//    FillVectorWithRandomNumbers(vector_with_random_numbers,
+//                                kTestSize,
+//                                kDistrBegin,
+//                                kDistrEnd);
+//
+//    auto start = std::chrono::high_resolution_clock::now();
+//    TestFunction(vector_with_random_numbers);
+//    auto stop = std::chrono::high_resolution_clock::now();
+//
+//    auto duration_in_seconds =
+//            std::chrono::duration_cast<std::chrono::seconds>(stop - start).count();
+//    std::cout << "Stress test took " << duration_in_seconds << " seconds" << std::endl;
+    // =============================================================
     return RUN_ALL_TESTS();
 }
